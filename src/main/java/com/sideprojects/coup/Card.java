7b95303ca601561c5fd3aeff3c614b22ce330db5
Jeff Crosley <jeffcrosley@gmail.com>
2020-02-08 11:06:25 -0500

    first commit

---
+++ src/main/java/com/sideprojects/coup/Card.java
@@ -0,0 +1,25 @@
+package com.sideprojects.coup;
+
+public class Card {
+	
+	private String suit = this.suit;
+	private boolean isFaceDown = this.isFaceDown;
+
+	public String getSuit() {
+		return this.suit;
+	}
+	
+	public boolean isFaceDown() {
+		return this.isFaceDown;
+	}
+	
+	public Card(String suit) {
+		this.suit = suit;
+		this.isFaceDown = true;
+	}
+	
+	private void flip() {
+		this.isFaceDown = !this.isFaceDown;
+	}
+	
+}
---
+++ src/main/java/com/sideprojects/coup/Main.java
@@ -0,0 +1,10 @@
+package com.sideprojects.coup;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
